70366dbee530e28b63cf4128bc8237a156e13f41
danHayworth <dan.d.hayworth@gmail.com>
2020-07-09 19:31:19 +1200

    adding pages

--- DjangoWebApp/flashAsPie/urls.py
+++ DjangoWebApp/flashAsPie/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import home, order, products
+from .views import home, order, products, employee
 
 urlpatterns = [
     path('', home, name='home'),
     path('products/', products, name='products'),
-    path('order/', order, name='order')
+    path('order/', order, name='order'),
+    path('employee/', employee, name='employee')
 ]
--- DjangoWebApp/flashAsPie/views.py
+++ DjangoWebApp/flashAsPie/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
-# Create your views here.
 from django.contrib.auth.decorators import login_required
-from .models import Product
+from .models import Product, User
 
 
 @login_required
 def home(request):
     products = Product.objects.all()
-
     context = {
         'products' : products
     }
 
     return render(request, "home.html", context)
 
-def products(request, product_id):
+def products(request):
     return render(request, "products.html", {})
 
-
+@login_required
 def order(request):
     return render(request, "order.html", {})
+
+
+@login_required
+def employee(request, user_id):
+    employee = User.objects.all()
+    return render(request, "employee.html", {})

c5c723983d751e2d7476cbf2f4e31868b7461f7b
danHayworth <dan.d.hayworth@gmail.com>
2020-07-09 21:04:31 +1200

    homepage details

--- DjangoWebApp/enviro/settings.py
+++ DjangoWebApp/enviro/settings.py
@@ -90,7 +90,7 @@ MEDIA_ROOT = os.path.join(DATA_DIR, 'media')
 STATIC_ROOT = os.path.join(DATA_DIR, 'static')
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'flashAsPie', 'static'),
+    os.path.join(BASE_DIR, 'flashAsPie', 'static', 'css', 'js'),
 )
 SITE_ID = 1
 

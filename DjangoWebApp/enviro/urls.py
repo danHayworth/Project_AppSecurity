b0d173d52cb99829e5687e72bbcdf5f479bdbb6f
danHayworth <dan.d.hayworth@gmail.com>
2020-07-10 22:13:00 +1200

    admin

--- DjangoWebApp/enviro/urls.py
+++ DjangoWebApp/enviro/urls.py
@@ -23,7 +23,7 @@ urlpatterns = [
 ] 
 
 urlpatterns += i18n_patterns(
-    url(r'^admin/', admin.site.urls),  # NOQA
+    url(r'^admin/', admin.site.urls, name='login'),  # NOQA
     url(r'^', include('cms.urls')),
 )
 
